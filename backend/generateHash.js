61bcdb684b4a32cccfa9799fe846d119ce554bf5
Manu080405 <manurejimon12@gmail.com>
2025-08-08 08:47:28 +0530

    Update generateHash.js

--- backend/generateHash.js
+++ backend/generateHash.js
@@ -1,7 +1,7 @@
 // generateHash.js
 import bcrypt from "bcryptjs";
 
-const password = "admin123";
+const password = "";
 
 bcrypt.hash(password, 10).then((hash) => {
   console.log("Hashed password:", hash);
